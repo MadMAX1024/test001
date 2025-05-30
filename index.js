df55d9e59a232e820e42753b8935cdbb285688b4
MadMAX1024 <35250538+MadMAX1024@users.noreply.github.com>
2025-05-30 00:17:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let mysql = require('mysql2');
 let connection =  mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"root",
+    password:"Root001!",
     database:"student_sys"
 })
 
